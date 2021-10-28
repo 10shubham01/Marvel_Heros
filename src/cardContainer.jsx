5344c4bccae5f22121857de7816b3087e03d9a31
10Shubham01 <shubham.gupta.17.1@mountblue.tech>
2021-10-28 19:24:26 +0530

    add card button and model added

--- src/cardContainer.jsx
+++ src/cardContainer.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import Card from "./card";
 import * as Marvel from "./API/api";
+import { Plus } from "react-feather";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
 
 class CardContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       Cards: [],
+      openModal: false,
     };
   }
+  onClickButton = (e) => {
+    e.preventDefault();
+    this.setState({ openModal: true });
+  };
+
+  onCloseModal = () => this.setState({ openModal: false });
   getCards = async () => {
     const Cards = await Marvel.getAllCards();
     this.setState({ Cards });
@@ -19,10 +29,22 @@ class CardContainer extends Component {
   render() {
     const { Cards } = this.state;
     return (
-      <div className="card-container">
-        {Cards.map((card) => (
-          <Card bkImage={card.img_Url} card={card} />
-        ))}
+      <div className="parent">
+        <div className="card-container">
+          {Cards.map((card) => (
+            <Card bkImage={card.img_Url} card={card} />
+          ))}
+        </div>
+        <div className="add-button">
+          <Plus size={48} onClick={this.onClickButton} />
+        </div>
+        <Modal
+          classNames="modal"
+          open={this.state.openModal}
+          onClose={this.onCloseModal}
+        >
+          <h1>hello</h1>
+        </Modal>
       </div>
     );
   }
