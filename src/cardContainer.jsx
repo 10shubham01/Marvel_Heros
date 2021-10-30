a55d4d7588b787c4a60086a48b02b6e8d95b318e
10Shubham01 <shubham.gupta.17.1@mountblue.tech>
2021-10-30 14:21:47 +0530

    preloader added

--- src/cardContainer.jsx
+++ src/cardContainer.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import Card from "./card";
 import * as Marvel from "./API/api";
-import { Plus } from "react-feather";
+import { LifeBuoy, Plus } from "react-feather";
 import { Modal } from "react-responsive-modal";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Spinner } from "react-bootstrap";
 import { Trash2 } from "react-feather";
 
 import { Link } from "react-router-dom";
@@ -22,6 +22,7 @@ class CardContainer extends Component {
       Did_You_Know: "",
       img_Url: "",
       searchValue: "",
+      dataLoad: false,
     };
   }
   /* ---------------------------- */
@@ -42,6 +43,7 @@ class CardContainer extends Component {
   getCards = async () => {
     const Cards = await Marvel.getAllCards();
     this.setState({ Cards });
+    this.setState({ dataLoad: true });
   };
   addCard = async (body) => {
     const newCard = await Marvel.createCard(body);
@@ -77,138 +79,169 @@ class CardContainer extends Component {
     this.getCards();
   }
   render() {
-    const { Cards } = this.state;
-    return (
-      <div className="parent">
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search..."
-            value={this.searchValue}
-            onKeyUp={this.handleSearch}
+    const { Cards, dataLoad } = this.state;
+    if (!dataLoad) {
+      return (
+        <div className="loader">
+          <img
+            src="https://i2.wp.com/boingboing.net/wp-content/uploads/2015/10/pJReN4H1.gif?w=970"
+            alt=""
           />
         </div>
-        <div className="card-container">
-          {Cards.filter((card) => {
-            if (this.state.searchValue === "") {
-              return card;
-            } else if (
-              card.Character_name.toLowerCase().includes(this.state.searchValue)
-            ) {
-              return card;
-            }
-          }).map((card) => (
-            <div className="card-parent">
-              <Link to={`/${card.id}`}>
-                <Card
-                  bkImage={card.img_Url}
-                  card={card}
-                  //   deleteCard={this.deleteCard}
-                  key={card.id}
-                />
-              </Link>
-              <div className="deletebutton">
-                <Trash2
-                  size={28}
-                  color="white"
-                  onClick={() => this.deleteCard(card.id)}
-                />
+      );
+    } else {
+      return (
+        <div className="parent">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.searchValue}
+              onKeyUp={this.handleSearch}
+            />
+          </div>
+          <div className="card-container">
+            {Cards.filter((card) => {
+              if (this.state.searchValue === "") {
+                return card;
+              } else if (
+                card.Character_name.toLowerCase().includes(
+                  this.state.searchValue
+                )
+              ) {
+                return card;
+              }
+            }).map((card) => (
+              <div className="card-parent">
+                <Link to={`/${card.id}`}>
+                  <Card
+                    bkImage={card.img_Url}
+                    card={card}
+                    //   deleteCard={this.deleteCard}
+                    key={card.id}
+                  />
+                </Link>
+                <div className="deletebutton">
+                  <Trash2
+                    size={28}
+                    color="white"
+                    onClick={() => this.deleteCard(card.id)}
+                  />
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="add-button">
-          <Plus size={48} onClick={this.onClickButton} />
-        </div>
-        <Modal
-          classNames="modal"
-          open={this.state.openModal}
-          onClose={this.onCloseModal}
-        >
-          <h3>Add Character</h3>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control
-                type="text"
-                placeholder="Character Name"
-                value={this.Character_name}
-                name="Character_name"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control
-                type="text"
-                placeholder="Super Powers"
-                value={this.Superpowers}
-                name="Superpowers"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control
-                type="text"
-                placeholder="Year_Created"
-                value={this.Year_Created}
-                name="Year_Created"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
+            ))}
+          </div>
+          <div className="add-button">
+            <Plus size={48} onClick={this.onClickButton} />
+          </div>
+          <Modal
+            classNames="modal"
+            open={this.state.openModal}
+            onClose={this.onCloseModal}
+          >
+            <h3>Add Character</h3>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Character Name"
+                  value={this.Character_name}
+                  name="Character_name"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Super Powers"
+                  value={this.Superpowers}
+                  name="Superpowers"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Year_Created"
+                  value={this.Year_Created}
+                  name="Year_Created"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
 
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Control
-                as="textarea"
-                rows={3}
-                placeholder="Description"
-                value={this.Description}
-                name="Description"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control
-                type="text"
-                placeholder="How he got his Power"
-                value={this.How_he_got_his_Power}
-                name="How_he_got_his_Power"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control
-                type="text"
-                placeholder="Did You Know"
-                value={this.Did_You_Know}
-                name="Did_You_Know"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control
-                type="text"
-                placeholder="Img Url"
-                value={this.img_Url}
-                name="img_Url"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Button
-              style={{
-                backgroundColor: "#EC1D24",
-                border: "none",
-                boxShadow: "inset 0 0 5px black",
-              }}
-              onClick={this.onAddCard}
-            >
-              Add Character
-            </Button>
-          </Form>
-        </Modal>
-      </div>
-    );
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Description"
+                  value={this.Description}
+                  name="Description"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="How he got his Power"
+                  value={this.How_he_got_his_Power}
+                  name="How_he_got_his_Power"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Did You Know"
+                  value={this.Did_You_Know}
+                  name="Did_You_Know"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Img Url"
+                  value={this.img_Url}
+                  name="img_Url"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button
+                style={{
+                  backgroundColor: "#EC1D24",
+                  border: "none",
+                  boxShadow: "inset 0 0 5px black",
+                }}
+                onClick={this.onAddCard}
+              >
+                Add Character
+              </Button>
+            </Form>
+          </Modal>
+        </div>
+      );
+    }
   }
 }
 
